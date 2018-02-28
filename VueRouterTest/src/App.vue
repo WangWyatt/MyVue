15f55279f542536692c97b754aecfebe5cb64a38
WangWyatt <wang_wyatt@outlook.com>
2018-03-01 00:31:15 +0800

    通过vue-cli脚手架配置vue

--- VueRouterTest/src/App.vue
+++ VueRouterTest/src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
-        <img src="./assets/logo.png"/>
-        <header>
-          <!--router-link 定义点击后导航到哪个路径下-->
-          <router-link to="/home">Home</router-link>
-          <router-link to="/about">About</router-link>
-
-        </header>
-        <router-view></router-view>
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
+
 <script>
 export default {
-  
+  name: 'App'
 }
 </script>
 
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
